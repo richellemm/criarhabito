c27c04f1f41e6c2cfa88d074fd2e04eb9b69b201
Richelle de Macêdo Monteiro <richellemonteiro@gmail.com>
2024-05-28 11:56:25 -0300

    JS original

--- index.js
+++ index.js
@@ -1,26 +1,27 @@
-function verificarTema() {
-    const modoEscuroAtivo = body.classList.contains("modo-escuro");
-    const modoEscuroAtivoA = header.classList.contains("modo-escuro");
-    const modoEscuroAtivoB = h1.classList.contains("modo-escuro");
-    const modoEscuroAtivoC = footer.classList.contains("modo-escuro");
-
-    if (modoEscuroAtivo && modoEscuroAtivoA && modoEscuroAtivoB && modoEscuroAtivoC) {
-        imagemBotaoTrocaTema.setAttribute("src", "./assets/imagens/sol.png");
-    } else {
-        imagemBotaoTrocaTema.setAttribute("src", "./assets/imagens/lua.png");
-    }
-}
-
-// Chamar a função para verificar o tema ao carregar a página
-window.addEventListener("load", verificarTema);
+const botaoAlterarTema = document.getElementById("botao-alterar-tema");
+const body = document.querySelector("body");
+const header = document.querySelector("header");
+const h1 = document.querySelector("h1");
+const footer = document.querySelector("footer");
+const imagemBotaoTrocaTema = document.querySelector(".imagem-botao");
 
 // evento botão
 botaoAlterarTema.addEventListener("click", () => {
+    const modoEscuroAtivo = body.classList.contains("modo-escuro")
+    const modoEscuroAtivoA = header.classList.contains("modo-escuro")
+    const modoEscuroAtivoB = h1.classList.contains("modo-escuro")
+    const modoEscuroAtivoC = footer.classList.contains("modo-escuro")
+    
     body.classList.toggle("modo-escuro");
     header.classList.toggle("modo-escuro");
     h1.classList.toggle("modo-escuro");
     footer.classList.toggle("modo-escuro");
 
-    // Verificar e aplicar o tema após a alteração do tema
-    verificarTema();
+    if(modoEscuroAtivo,modoEscuroAtivoA,modoEscuroAtivoB, modoEscuroAtivoC){
+         imagemBotaoTrocaTema.setAttribute("src", "./assets/imagens/sol.png")
+
+    }else{
+       imagemBotaoTrocaTema.setAttribute("src", "./assets/imagens/lua.png")
+    
+    }
 });
