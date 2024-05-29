cafbe4b9a9b358961468cf8ccf1053f53dfcc9a3
juliamenzss <juliamenzss@gmail.com>
2024-05-29 18:55:01 -0300

    Ajustando modo escuro

--- main.js
+++ main.js
@@ -1,13 +1,29 @@
 let modo = document.getElementById("modo");
+let mudarPagina = document.querySelector(".mudarPagina");
 let body = document.querySelector("body");
 let main = document.querySelector("main");
 let header = document.querySelector("header");
 let footer = document.querySelector("footer");
 
-modo.addEventListener("click", () => {
-  modo.classList.toggle("dark");
-  body.classList.toggle("dark");
-  main.classList.toggle("dark");
-  header.classList.toggle("dark");
-  footer.classList.toggle("dark");
+
+
+document.addEventListener("DOMContentLoaded", () => {
+  function escuro() {
+    modo.classList.toggle("dark");
+    body.classList.toggle("dark");
+    main.classList.toggle("dark");
+    header.classList.toggle("dark");
+    footer.classList.toggle("dark");
+  }
+
+  if (localStorage.getItem("modoEscuro") === "true") {
+    escuro();
+  }
+
+  modo.addEventListener("click", () => {
+    escuro();
+    localStorage.setItem("modoEscuro", body.classList.contains("dark"));
+  });
 });
+
+
